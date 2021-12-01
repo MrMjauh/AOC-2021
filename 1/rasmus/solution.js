65f797811c3d59936ceae54643ba58a76b639079
Rasmus Eriksson <rasmus@mutate.se>
2021-12-01 11:44:40 +0100

    Add solution for day 1

---
+++ 1/rasmus/solution.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const os = require('os');
+
+const input = fs.readFileSync("input", "utf-8");
+const readings = input.split(os.EOL).map(readingStr => parseInt(readingStr));
+
+function getAccumulatedReading(readings, pos, size) {
+    let sum = 0;
+    for (let i = pos; i < (pos + size); i++) {
+        sum += readings[i];
+    }
+    return sum;
+}
+
+function getNumberOfIncreasedReadings(readings, windowSize) {
+    let numberOfIncreasedReadings = 0;
+    for (let i = 1; i <= (readings.length - windowSize); i++) {
+        if (getAccumulatedReading(readings, i, windowSize) > getAccumulatedReading(readings, i - 1, windowSize)) {
+            numberOfIncreasedReadings++;
+        }
+    }
+    return numberOfIncreasedReadings;
+}
+
+console.log(`Total readings ${readings.length}`)
+console.log(`part 1 > ${getNumberOfIncreasedReadings(readings, 1)}`);
+console.log(`part 2 > ${getNumberOfIncreasedReadings(readings, 3)}`);
